e890ddf72d2c5860c9054b5b0446521d3a6bb558
simon-m-dignite <shoaib.muhammad@launchbox.pk>
2024-04-23 01:41:43 +0500

    added terms and conditions page

--- src/components/global/Navbar.jsx
+++ src/components/global/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
         <img src={Logo} alt="" className="w-[146px] h-[39px]" />
       </div>
       <div className="hidden lg:flex items-center gap-x-16 mr-auto">
-        <NavLink label={"Home"} id={"#home"} />
+        <NavLink label={"Home"} id={"/"} />
         <NavLink label={"Introduction"} id={"#introduction"} />
         <NavLink label={"Features"} id={"#features"} />
         <NavLink label={"Contact Us"} id={"#contact"} />
@@ -26,7 +26,7 @@ const Navbar = () => {
         <div className="bg-white h-auto w-48 absolute top-16 rounded-2xl right-4 float-end p-4 shadow-2xl">
           
           <div className="w-full flex flex-col gap-y-4 py-2 ps-1">
-            <NavLink label={"Home"} id={"#home"} />
+            <NavLink label={"Home"} id={"/"} />
             <NavLink label={"Introduction"} id={"#introduction"} />
             <NavLink label={"Features"} id={"#features"} />
             <NavLink label={"Contact Us"} id={"#contact"} />
