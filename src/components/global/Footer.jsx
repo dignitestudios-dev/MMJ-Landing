bc67bf90ca0081c5212fbc7bf033a83a2d043e0e
simon-m-dignite <shoaib.muhammad@launchbox.pk>
2024-04-30 03:29:21 +0500

    changed footer icon

--- src/components/global/Footer.jsx
+++ src/components/global/Footer.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Logo } from "../../assets/export";
-import { FaFacebookF } from "react-icons/fa";
-import { FaTwitter } from "react-icons/fa";
+import { FaFacebookF, FaInstagram } from "react-icons/fa";
 import { FaLinkedinIn } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
@@ -15,13 +14,13 @@ const Footer = () => {
         <div className="col-span-12 md:col-span-4 flex flex-col justify-start items-start lg:items-center gap-y-4">
           <p className="text-xl xl:text-2xl font-semibold leading-[27.72px]">Find us on Social Media</p>
           <div className="flex items-center justify-center gap-x-6">
-            <Link to="/" className="w-11 h-11 border-[1px] border-black rounded-full flex justify-center items-center">
+            <Link to="https://www.facebook.com" className="w-11 h-11 border-[1px] border-black rounded-full flex justify-center items-center">
               <FaFacebookF className="w-5 h-5" />
             </Link>
-            <Link to="/" className="w-11 h-11 border-[1px] border-black rounded-full flex justify-center items-center">
-              <FaTwitter className="w-5 h-5" />
+            <Link to="https://www.instagram.com" className="w-11 h-11 border-[1px] border-black rounded-full flex justify-center items-center">
+              <FaInstagram className="w-5 h-5" />
             </Link>
-            <Link to="/" className="w-11 h-11 border-[1px] border-black rounded-full flex justify-center items-center">
+            <Link to="https://www.linkedin.com" className="w-11 h-11 border-[1px] border-black rounded-full flex justify-center items-center">
               <FaLinkedinIn className="w-5 h-5" />
             </Link>
           </div>
