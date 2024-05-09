8f0366cf0b9246bd6f641fba20b80f3446b64c6a
simon-m-dignite <shoaib.muhammad@launchbox.pk>
2024-05-09 22:20:35 +0500

    mobile optimization

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <div className={`px-4 md:px-8 h-auto `} id="home">
       <div className="w-full rounded-[4rem] lg:h-[603px] grid grid-cols-1 md:grid-cols-12 gap-y-0 hero-bg">
-        <div className="col-span-12 lg:col-span-6 flex flex-col items-start justify-center gap-y-14 px-8 py-8 md:px-16">
+        <div className="col-span-12 lg:col-span-6 flex flex-col items-start justify-center gap-y-7 md:gap-y-14 px-8 py-8 md:px-16">
           <div className="flex flex-col gap-y-6">
             <h1 className="text-[35px] md:text-[55px] font-normal text-white leading-[40px] md:leading-[58px]">
               <span className={`${styles.greenText} font-bold`}>Discover</span>{" "}
@@ -24,10 +24,10 @@ const Hero = () => {
           </div>
           <div className="flex items-center justify-start gap-x-2 md:gap-x-6">
             <Link to="/" className="">
-              <img src={GooglePlay} alt="" className="h-14" />
+              <img src={GooglePlay} alt="" className="h-16 lg:h-14" />
             </Link>
             <Link to="https://apps.apple.com/us/app/my-medical-journal-app/id6502008545" target="_blank" className="">
-              <img src={AppStore} alt="" className="h-14" />
+              <img src={AppStore} alt="" className="h-16 lg:h-14" />
             </Link>
           </div>
         </div>
--- src/components/Home/StartYourJourney.jsx
+++ src/components/Home/StartYourJourney.jsx
@@ -11,7 +11,7 @@ const StartYourJourney = () => {
     <div className="w-full py-6 md:py-20 px-4 lg:px-16">
       <div className="w-full h-full grid grid-cols-1 lg:grid-cols-12 gap-y-6 lg:gap-y-0">
         <div className="col-span-12 lg:col-span-6 flex flex-col items-start justify-center gap-y-4 xl:ps-24 text-justify order-2 lg:order-1">
-          <h2 className="text-[32px] font-semibold leading-[48px]">
+          <h2 className="text-3xl md:text-[32px] font-semibold leading-[48px]">
             Start Your Journey Today
           </h2>
           <p className={`font-normal text-[#6F6F6F] text-lg leading-[27px]`}>
@@ -22,10 +22,10 @@ const StartYourJourney = () => {
           </p>
           <div className="flex items-center gap-4 pt-2">
             <Link to="/">
-              <img src={GooglePlay} alt="" className="" />
+              <img src={GooglePlay} alt="" className="h-14" />
             </Link>
             <Link to="https://apps.apple.com/us/app/my-medical-journal-app/id6502008545" target="_blank">
-              <img src={AppStore} alt="" className="" />
+              <img src={AppStore} alt="" className="h-14" />
             </Link>
           </div>
         </div>
