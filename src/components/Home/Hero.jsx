9abe470e67be7f69bf755446e1630237fd775306
simon-m-dignite <shoaib.muhammad@launchbox.pk>
2024-05-09 22:08:12 +0500

    changed app store and play store images

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <div className={`px-4 md:px-8 h-auto `} id="home">
       <div className="w-full rounded-[4rem] lg:h-[603px] grid grid-cols-1 md:grid-cols-12 gap-y-0 hero-bg">
-        <div className="col-span-12 lg:col-span-6 flex flex-col items-start justify-evenly gap-y-6 px-8 py-8 md:px-16">
+        <div className="col-span-12 lg:col-span-6 flex flex-col items-start justify-center gap-y-14 px-8 py-8 md:px-16">
           <div className="flex flex-col gap-y-6">
             <h1 className="text-[35px] md:text-[55px] font-normal text-white leading-[40px] md:leading-[58px]">
               <span className={`${styles.greenText} font-bold`}>Discover</span>{" "}
@@ -22,34 +22,14 @@ const Hero = () => {
               Data Analysis
             </p>
           </div>
-          {/* <div className="flex flex-col gap-1">
-            <p className={`text-white font-bold text-4xl`}>
-              Launching Soon!
-            </p>
-          </div> */}
-          <div className="flex items-center justify-start gap-x-0 md:gap-x-6">
-            <Link to="/" target="_blank" className="">
-              <img src={GooglePlay} alt="" className="" />
+          <div className="flex items-center justify-start gap-x-2 md:gap-x-6">
+            <Link to="/" className="">
+              <img src={GooglePlay} alt="" className="h-14" />
             </Link>
             <Link to="https://apps.apple.com/us/app/my-medical-journal-app/id6502008545" target="_blank" className="">
-              <img src={AppStore} alt="" className="" />
+              <img src={AppStore} alt="" className="h-14" />
             </Link>
           </div>
-          
-          {/* <div className="flex items-center gap-x-6 md:gap-x-16 md:ps-2">
-            <div>
-              <p className="text-white text-3xl font-bold">4.8</p>
-              <p className={`text-base font-normal ${styles.secondaryText}`}>
-                Rating in Appstore
-              </p>
-            </div>
-            <div>
-              <p className="text-white text-[30px] font-bold">500K+</p>
-              <p className={`text-base font-normal ${styles.secondaryText}`}>
-                Active users
-              </p>
-            </div>
-          </div> */}
         </div>
         <div className="col-span-12 lg:col-span-6 flex justify-center items-end md:bg-transparent">
           <img src={HeroImage} alt="" className="2xl:h-[96%]"/>
